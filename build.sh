1e89c58db370475fca42778bb468faafaf79fc35
Nathaniel D'Rozario <nsdrozario@gmail.com>
2020-06-29 21:02:21 -0700

    added autotool support and it might not work

--- build.sh
+++
@@ -1,6 +0,0 @@
-# In my opinion this project doesn't even need a makefile
-# this script assumes you are in the root directory of this repository
-
-g++ src/wavecmp.cpp -c -o func.o -Iinclude/
-g++ src/func.cpp -c -o wavecmp.o -Iinclude/
-g++ wavecmp.o func.o -o wavecmp.exe
