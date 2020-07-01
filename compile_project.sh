587b6a2ee539bcfc24b9cf290f53f4bb8fd3dd52
Nathaniel D'Rozario <nsdrozario@gmail.com>
2020-06-30 20:39:53 -0700

    fixed another typo in compile project script

--- compile_project.sh
+++ compile_project.sh
@@ -2,5 +2,5 @@
 
 g++ src/wavecmp.cpp -c -o wavecmp.o -Iinclude/ `pkg-config gtkmm-3.0 --cflags --libs`
 g++ src/func.cpp -c -o func.o -Iinclude/
-g++ wavecmp.o func.o wavecmp `pkg-config gtkmm-3.0 --cflags --libs`
+g++ wavecmp.o func.o -o wavecmp `pkg-config gtkmm-3.0 --cflags --libs`
 
