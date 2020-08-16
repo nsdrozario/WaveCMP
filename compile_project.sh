02eb784adf1956a39fa8482d511879f7342acb18
Nathaniel D'Rozario <48631513+nsdrozario@users.noreply.github.com>
2020-08-16 01:51:30 -0700

    Delete compile_project.sh

--- compile_project.sh
+++
@@ -1,6 +0,0 @@
-# help me please
-
-g++ src/wavecmp.cpp -c -o wavecmp.o -Iinclude/ `pkg-config gtkmm-3.0 --cflags --libs`
-g++ src/func.cpp -c -o func.o -Iinclude/
-g++ wavecmp.o func.o -o wavecmp `pkg-config gtkmm-3.0 --cflags --libs`
-
